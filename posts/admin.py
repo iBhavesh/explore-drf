deb0caf4a441629e5888f7a503e2c42d45cb3d25
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-06-23 23:33:56 +0000

    update models

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Posts, Comments
 # Register your models here.
-admin.site.register(Posts)
-admin.site.register(Comments)
+
+
+@admin.register(Posts)
+class PostsManager(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comments)
+class CommentsManager(admin.ModelAdmin):
+    pass
--- posts/models.py
+++ posts/models.py
@@ -1,26 +1,43 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-# Create your models here.
+from user.models import User
 
 
 class Posts(models.Model):
-    author = models.ForeignKey("user.User", verbose_name=_(
+    content_option = [
+        ('post', 'post'),
+        ('profile', 'profile')
+    ]
+
+    author = models.ForeignKey(User, verbose_name=_(
         "Author"), on_delete=models.CASCADE)
-    text = models.TextField(_("Text"))
-    media = models.FileField(_("media"), upload_to=None,
-                             max_length=100, null=True)
+    caption = models.TextField(_("Caption"))
+    media_path = models.FileField(_("Media Path"), upload_to='posts',
+                                  max_length=255, null=True)
     media_type = models.CharField(_("Media Type"), max_length=50, null=True)
+    content_type = models.CharField(
+        _("Content type"), max_length=50, choices=content_option)
+    created_at = models.DateTimeField(
+        _("Created At"), auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(
+        _("Updated At"), auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.caption[:10] + "..."
 
     class Meta:
         verbose_name_plural = "Posts"
 
 
 class Comments(models.Model):
-    post = models.ForeignKey("posts.Posts", verbose_name=_(
+    post = models.ForeignKey(Posts, verbose_name=_(
         "Comments"), on_delete=models.CASCADE)
     is_active = models.BooleanField(_("Is Active"), default=True)
     comment = models.TextField(_("Comment"))
+    created_at = models.DateTimeField(
+        _("Created At"), auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(
+        _("Updated At"), auto_now=False, auto_now_add=False)
 
     class Meta:
         verbose_name_plural = "Comments"
