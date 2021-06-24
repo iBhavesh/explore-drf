b190c51393b7de6f0116c3c50238d7ca7087b2a2
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-06-24 09:32:24 +0000

    Add new serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Posts
+from .models import Comments, Posts
 
 
-class PostSerializer(serializers.Serializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posts
+        fields = ['author', 'caption', 'media_type',
+                  'content_type', 'media_path']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    
+    class Meta:
+        model = Comments
+        fields = ['post', 'comment', 'created_at']
+        extra_kwargs = {'is_active': {'write_only': True}}
--- user/serializers.py
+++ user/serializers.py
@@ -6,8 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name',
-                  'last_name', 'date_of_birth', 'password']
+                  'last_name', 'date_of_birth', 'password', 'followers']
         extra_kwargs = {'password': {'write_only': True}}
+        depth = 1
 
     def create(self, validated_data):
         user = User.objects.create_user(
