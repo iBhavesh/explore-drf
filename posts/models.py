b00d59fbbadedfd0b27f2ceb70abb33ee4889f66
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-06-25 20:52:13 +0000

    add compress image feature

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from time import strftime
+import mimetypes
 import os
 import sys
 from io import BytesIO
@@ -10,7 +11,6 @@ from user.models import User
 
 
 def upload_to(instance, filename):
-    print(instance, filename)
     filenames = os.path.splitext(filename)
     return "posts/" + strftime('%Y%m%d%H%M%S') + filenames[-1]
 
@@ -50,20 +50,28 @@ class Posts(models.Model):
             print('File Could not be deleted')
         return super().delete(using=using, keep_parents=keep_parents)
 
-    # def save(self, *args, **kwargs):
-    #     if not self.id:
-    #         self.media = self.compressImage(self.uploadedImage)
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.media = self.compress_image(self.media)
+        super().save(*args, **kwargs)
 
-    def compressImage(self, uploadedImage):
-        imageTemproary = Image.open(uploadedImage)
-        outputIoStream = BytesIO()
-        imageTemproaryResized = imageTemproary.resize((1020, 573))
-        imageTemproary.save(outputIoStream, format='JPEG', quality=60)
-        outputIoStream.seek(0)
-        uploadedImage = InMemoryUploadedFile(outputIoStream, 'ImageField', "%s.jpg" % uploadedImage.name.split('.')[
-                                             0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
-        return uploadedImage
+    def compress_image(self, uploaded_image):
+        mime_type = mimetypes.guess_type(uploaded_image.name)
+        if mime_type[0] is not None:
+            return uploaded_image
+        elif not mime_type[0].startswith('image'):
+            return uploaded_image
+        elif uploaded_image.size is not None and uploaded_image.size / 1024 < 200:
+            return uploaded_image
+        image_temporary = Image.open(uploaded_image)
+        output_io_stream = BytesIO()
+        # imageTemproaryResized = imageTemproary.resize((1020, 573))
+        image_temporary.save(output_io_stream, format='JPEG', quality=50)
+        output_io_stream.seek(0)
+        uploaded_image = InMemoryUploadedFile(
+            output_io_stream, 'ImageField', "%s.jpg" % uploaded_image.name.split('.')[
+                0], 'image/jpeg', sys.getsizeof(output_io_stream), None)
+        return uploaded_image
 
     class Meta:
         verbose_name_plural = "Posts"
