eef001938d39fef80c9d4c2d0813e44da44ac06b
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-06-28 21:40:52 +0000

    Complete post module

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AddPost.as_view(), name="add-post"),
+    path('', views.PostList.as_view(), name="post_list"),
+    path('<int:pk>', views.Post.as_view(), name="post_retrieve_update_destroy"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,16 @@
 from django.db.models import Q
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
 from rest_framework.response import Response
+from posts.permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 from .models import Posts
 
 # Create your views here.
 
 
-class AddPost(ListCreateAPIView):
+class PostList(ListCreateAPIView):
     parser_classes = [MultiPartParser, FormParser]
     serializer_class = PostSerializer
 
@@ -26,7 +27,14 @@ class AddPost(ListCreateAPIView):
                             status=status.HTTP_401_UNAUTHORIZED)
         return super().create(request, *args, **kwargs)
 
-    # def perform_create(self, serializer):
-    #     if self.request.data['author'] is not self.request.user.id:
-    #         raise ValidationError("You are not authorized to do this")
-    #     serializer.save(author=self.request.user)
+
+class Post(RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    parser_classes = [MultiPartParser, FormParser]
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        user_id = self.request.user.id
+        return Posts.objects.filter(~Q(author=user_id),
+                                    Q(author__is_private_profile=False) |
+                                    Q(author__followed_by=user_id))
